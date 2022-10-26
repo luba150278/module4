52205d3d59c50c472dfad2819827a0bcc2f8d37a
Lyubov Myetolkina <luba150278@gmail.com>
2022-10-26 18:11:24 +0300

    aa

--- src/components/CheckTypes/Data.js
+++ src/components/CheckTypes/Data.js
@@ -25,3 +25,32 @@ Data.propTypes = {
 }
 
 export default Data;
+
+
+// import PropTypes from "prop-types";
+
+// function Data({ title, body, arr }) {
+//   return (
+//     <div className="container">
+//       <h2>{title}</h2>
+//       <p>{body}</p>
+//       <ul>
+//         {arr.map((item) => (
+//           <li key={item}>{item}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+
+// Data.defaultProps = {
+//   title: "Незнакомец",
+//   arr2: []
+// };
+// Data.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   body: PropTypes.string,
+//   arr: PropTypes.arrayOf(PropTypes.number),
+//   arr2: PropTypes.arrayOf(PropTypes.number).isRequired,
+// };
